376111d5c816fe087e3c89ff443e833b697c8148
enenrico <enrico.bolze@arcor.de>
2020-02-12 08:04:20 +0100

    Create power_saving_mode.bat

---
+++ power_saving_mode.bat
@@ -0,0 +1,13 @@
+rem---------------------------------------------------------------------
+rem Datei power_saving_mode.bat
+rem set the monitor timout at 5h 
+
+rem---------------------------------------------------------------------
+POWERCFG -Change -monitor-timeout-ac 300
+POWERCFG -Change -monitor-timeout-dc 300
+rem---------------------------------------------------------------------
+
+rem set the standby time at 5h
+rem---------------------------------------------------------------------
+POWERCFG -Change -standby-timeout-ac 300
+POWERCFG -Change -standby-timeout-dc 300
